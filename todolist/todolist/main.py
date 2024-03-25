198c4cebe777a6e7d6aa671998725bc18ce981d3
saeedurrehman01 <saeedrehman241@gmail.com>
2024-03-25 15:50:56 +0500

    Class 1

--- todolist/todolist/main.py
+++ todolist/todolist/main.py
@@ -3,20 +3,49 @@ import uvicorn
 
 app = FastAPI()
 
-@app.get("/gettodos/{id}")
-def gettodos(id):
-    print ("get todos called")
-    return ("message: Test Todo List")
+students = [{
+    "userName":"Ali",
+    "rollNo": 2342
+},
+            {
+    "userName":"Naveed Sarwar",
+    "rollNo": 9213
+}
+            ]
 
-@app.post("/get_todos")
-def get_todos():
-    print ("get Post todos called")
-    return ("message: Test Post Todo List")
+@app.get("/students")
+def getStudents():
+    return students
+
+@app.get("/addStudent")
+def addStudent(userName:str, rollNo:str):
+    global students
+    students.append({"userName":userName, "rollNo":rollNo})
+    return students
+
+@app.get("/")
+def helloWorld():
+    return {"hello": "world"}
+
+@app.get("/gettodolist/{userName}/{rollNo}")
+def gettodolist(userName:str, rollNo:str):
+    print("Get todolist called",userName,rollNo)
+    return userName + rollNo 
+
+@app.post("/gettodolist")
+def gettodolistPost():
+    print("Get post method todolist called")
+    return "post gettodolist called"
 
 @app.get("/getSingleTodo")
-def getSingleTodo():
-    print("Get todo Called")
-    return "getSingleTodo called"
+def getSingleTodo(userName:str, rollNo:str):
+    print("Get todo called",userName,rollNo )
+    return "getSingleTodo called sadf sd sdff"
 
-uvicorn.run("todolist.main:app", host="local", port=8080, reload=True )
+@app.put("/updateTodo")
+def updateTodo():
+    return "updateTodo called"
+    
 
+def start():
+    uvicorn.run("todolist.main:app",host="127.0.0.1", port=8080, reload=True)
